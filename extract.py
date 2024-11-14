ed457f139ce6bba80a94f23ed5fd11b496894144
tyreseGill <168314093+tyreseGill@users.noreply.github.com>
2024-11-14 00:07:49 -0800

    Pulls Up Command for Input of Passphrase
    Issue in that this code should input the passphrase given into the command prompt but it does not do so.

--- extract.py
+++ extract.py
@@ -1,27 +1,67 @@
 import subprocess
+#import wexpect
 
 
-def extract_message(image_path: str, password: str) -> str:
+def extract_message(cover_file: str, passphrase: str) -> str:
     """
     Executes commandlines to run Steghide and obtain in order
     to obtain the "secret" message embedded within a given image.
-    :param image_path: The location on the computer where the image to be
-        examined is located.
-    :param password: The key used to decrypt and access the "secret" message
+    :param cover_file: Represents the image to be examined for a message.
+    :param passphrase: The key used to decrypt and access the "secret" message
         in the image.
-    :return: A string representing the secret text from the image
+    :return: The text that was embedded into an image.
     """
-    if image_path == "download.jpg":
-        extracted_data_file_name = "key.txt"
-    else:
-        extracted_data_file_name = "code.txt"
+    #extracted_file: str = ""
+    extracted_file_path: str = "C:\\Users\..."
 
-    result = subprocess.run(["steghide", "extract", "-sf", image_path, "-xf", extracted_data_file_name], input=password,
-                            text=True, capture_output=True)
-    with open(extracted_data_file_name, "r") as file:
-        contents = file.read()
-    print("Extraction Successful with: " + result.stdout)
+    if cover_file.endswith(".jpg"):
+        if cover_file.endswith("download.jpg"):  # image containing the key
+        #if cover_file == "White_shark.jpg":
+            extracted_file_path += "\key.txt"
+    else:  # our bmp containing encrypt.py
+        extracted_file_path += "\code.txt"
 
+    steghide_path = "C:\\Users\...\steghide-0.5.1-win32\steghide\steghide.exe"
+
+    print("Cover file:", cover_file)
+    print("File being written to:", extracted_file_path)
+
+    #command = ["start", "cmd", "/k", steghide_path, "extract", "-sf", cover_file, "-xf", extracted_file_path]
+    #command = ["start", "cmd", "/k", steghide_path, "extract", "-sf", cover_file]
+    command = f'start cmd /k {steghide_path} extract -sf "{cover_file}"'
+    #command = f'echo {passphrase} | "{steghide_path}" extract -sf "{cover_file}"'
+    #command =["echo", passphrase, "|", steghide_path, "extract", "-sf", cover_file]
+
+    try:
+        result = subprocess.run(
+            # f"start cmd /K 'steghide extract -sf {cover_file}'",
+            #["cmd", "/c", command],
+            command,
+            input=passphrase,
+            text=True,
+            shell=True)
+        #result = subprocess.run(command, shell=True, capture_output=True, text=True)
+        print(result)
+
+        if cover_file.endswith(".jpg" or ".png"):
+            with open(extracted_file_path, "r") as file:
+                contents = file.read()
+        elif cover_file.endswith(".bmp"):
+            with open(extracted_file_path, "rb") as file:
+                contents = file.read()
+        else:
+            print("Unsupported file detected!")
+            return 1
+        print("Extraction Successful with: " + contents)
+    except FileNotFoundError as fnfe:
+        print(f"The file {fnfe.filename} was not found anywhere!")
+    except Exception as e:
+        print(e)
 
     # Returns the code/key in a string format
     return contents
+
+
+extract_message("download.jpg", "Mochi\n")
+
+#extract_message("download2.bmp", "Mochi\n")
